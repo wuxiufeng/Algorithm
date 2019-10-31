07eae2b7cc992decfa47566302ba9d9f8b190f93
lvjunsetup <lvjun.up@qq.com>
2019-10-31 10:06:14 +0800

    init

--- Test/src/CT3/Main3.java
+++ Test/src/CT3/Main3.java
@@ -8,85 +8,43 @@ import java.util.Scanner;
  * @description: TODO
  * @author: Jun
  * @version: 1.0.0
- * @create: 2019-10-31 09:45
+ * @create: 2019-10-31 10:02
  */
 public class Main3 {
-    static String KMPSearch(String pat, String txt) {
-        StringBuilder result = new StringBuilder();
-        int M = pat.length();
-        int N = txt.length();
-
-        // create lps[] that will hold the longest
-        // prefix suffix values for pattern
-        int[] lps = new int[M];
-        int j = 0; // index for pat[]
-
-        // Preprocess the pattern (calculate lps[]
-        // array)
-        computeLPSArray(pat, M, lps);
-
-        int i = 0; // index for txt[]
-        while (i < N) {
-            if (pat.charAt(j) == txt.charAt(i)) {
-                j++;
-                i++;
-            }
-            if (j == M) {
-                result.append(i - j).append(" ");
-                j = lps[j - 1];
-            }
-
-            // mismatch after j matches
-            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
-                // Do not match lps[0..lps[j-1]] characters,
-                // they will match anyway
-                if (j != 0)
-                    j = lps[j - 1];
-                else
-                    i = i + 1;
-            }
-        }
-        return new String(result).trim();
-    }
-
-    static void computeLPSArray(String pat, int M, int[] lps) {
-        // length of the previous longest prefix suffix
-        int len = 0;
-        int i = 1;
-        lps[0] = 0; // lps[0] is always 0
-
-        // the loop calculates lps[i] for i = 1 to M-1
-        while (i < M) {
-            if (pat.charAt(i) == pat.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else // (pat[i] != pat[len])
-            {
-                // This is tricky. Consider the example.
-                // AAACAAAA and i = 7. The idea is similar
-                // to search step.
-                if (len != 0) {
-                    len = lps[len - 1];
-
-                    // Also, note that we do not increment
-                    // i here
-                } else // if (len == 0)
-                {
-                    lps[i] = len;
-                    i++;
-                }
-            }
-        }
-    }
-
-    // Driver program to test above function
     public static void main(String[] args) {
-        Scanner cin = new Scanner(System.in);
-        int caseNum = Integer.parseInt(cin.nextLine().trim());
-        for (int curi = 0; curi < caseNum; curi++) {
-            String[] input = cin.nextLine().trim().split(",");
-            System.out.printf("%s%n", KMPSearch(input[1], input[0]));
+        String[] unit = {"1", "2", "3", "4", "5", "$", "5", "4", "3", "2", "1"};
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int query = sc.nextInt();
+
+            while (query > unit.length) {
+                int[] domainInfo = domainInfo(query);
+                int domainStart = ((domainInfo[0] - domainInfo[1]) / 2) + domainInfo[1];
+
+                if (query <= domainStart) {
+                    query = 6; //$
+                    break;
+                }
+                query -= domainStart;
+            }
+            System.out.println(query > 0 ? unit[query - 1] : "");
+            t--;
+        }
+    }
+
+    public static int[] domainInfo(int query) {
+        int domainSize = 5;
+        int ith = 0;
+
+        while (true) {
+            if (query <= domainSize) {
+                return new int[]{domainSize, ith};
+            }
+            ith++;
+            domainSize = (domainSize * 2) + ith;
         }
     }
 }
---
+++ Test/src/CT3/Main4.java
@@ -0,0 +1,92 @@
+package CT3;
+
+import java.util.Scanner;
+
+/**
+ * @program: Test
+ * @ClassName Main3.java
+ * @description: TODO
+ * @author: Jun
+ * @version: 1.0.0
+ * @create: 2019-10-31 09:45
+ */
+public class Main4 {
+    static String KMPSearch(String pat, String txt) {
+        StringBuilder result = new StringBuilder();
+        int M = pat.length();
+        int N = txt.length();
+
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int[] lps = new int[M];
+        int j = 0; // index for pat[]
+
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+
+        int i = 0; // index for txt[]
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                result.append(i - j).append(" ");
+                j = lps[j - 1];
+            }
+
+            // mismatch after j matches
+            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        return new String(result).trim();
+    }
+
+    static void computeLPSArray(String pat, int M, int[] lps) {
+        // length of the previous longest prefix suffix
+        int len = 0;
+        int i = 1;
+        lps[0] = 0; // lps[0] is always 0
+
+        // the loop calculates lps[i] for i = 1 to M-1
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else // (pat[i] != pat[len])
+            {
+                // This is tricky. Consider the example.
+                // AAACAAAA and i = 7. The idea is similar
+                // to search step.
+                if (len != 0) {
+                    len = lps[len - 1];
+
+                    // Also, note that we do not increment
+                    // i here
+                } else // if (len == 0)
+                {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+
+    // Driver program to test above function
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int caseNum = Integer.parseInt(cin.nextLine().trim());
+        for (int curi = 0; curi < caseNum; curi++) {
+            String[] input = cin.nextLine().trim().split(",");
+            System.out.printf("%s%n", KMPSearch(input[1], input[0]));
+        }
+    }
+}
